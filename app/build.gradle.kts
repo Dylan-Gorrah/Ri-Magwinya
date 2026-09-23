757681dfda65e63e522448e36d6fef8c89b60ce2
Dylan-Gorrah <dylangorrah3@gmail.com>
2026-09-23 02:26:40 +0200

    release: 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         targetSdk = 36
         // Bump versionCode for every upload to Play; versionName is what
         // people see.
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
